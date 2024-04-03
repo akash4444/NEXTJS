b12e657f84b625c5ce797013b573984020c5a8d1
akashkale44 <akashkumarkale4@gmail.com>
2024-04-03 22:50:20 +0530

    added new pdf changes-pdfmake

--- my-app/src/app/Invoice/Invoice.jsx
+++ my-app/src/app/Invoice/Invoice.jsx
@@ -1,107 +1,103 @@
 "use client";
-import React, { useRef, useState } from "react";
-import html2canvas from "html2canvas";
+import React from "react";
 import moment from "moment";
-import { Modal, Button, IconButton } from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
-import { LoadingSpinner, AlertModal } from "../CommonComponents";
 import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
 
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
-
-const PDFGenerator = ({
-  order = {},
-  userId = "",
-  userName = "",
-  orderDownloading = "",
-  setOrderDownloading = () => {},
-}) => {
-  const htmlRef = useRef(null);
-  const [openInvoice, setOpenInvoice] = useState(false);
-
-  let invoiceData = {};
-  if (order?.status === "delivered") {
-    const productSubtotals = (order) => {
-      return order.map((item) => item.price * item.quantity);
-    };
-
-    // Function to calculate total bill
-    const totalBill = (order) => {
-      return productSubtotals(order).reduce(
-        (acc, subtotal) => acc + subtotal,
-        0
-      );
-    };
-
-    // Construct order details object
-    invoiceData = {
-      customerName: userName,
-      items: order.items,
-      orderId: order._id,
-      createdAt: order.createdAt,
-      totalAmount: totalBill(order.items),
-      deliveryAddress: order.deliveryAddress,
-    };
-  }
-
-  const handleDownloadInvoice = async () => {
-    // try {
-    //   setOrderDownloading(order._id);
-    //   const pdfdata = document.getElementById("pdf-creation");
-    //   html2canvas(pdfdata, { scrollY: -window.scrollY }).then(function (
-    //     canvas
-    //   ) {
-    //     var imgData = canvas.toDataURL("image/png");
-    //     var imgWidth = 210;
-    //     var pageHeight = 295;
-    //     var imgHeight = (canvas.height * imgWidth) / canvas.width;
-    //     var heightLeft = imgHeight;
-    //     var doc = new jsPDF("p", "mm");
-    //     var position = 10; // give some top padding to first page
-    //     doc.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
-    //     heightLeft -= pageHeight;
-    //     while (heightLeft >= 0) {
-    //       position = heightLeft - imgHeight + 10; // top padding for other pages
-    //       doc.addPage();
-    //       doc.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
-    //       heightLeft -= pageHeight;
-    //     }
-    //     doc.save(
-    //       `invoice-${order._id}-${moment().format("YYYY-MM-DD_HH-mm-ss")}.pdf`
-    //     );
-    //   });
-    //   setOrderDownloading("");
-    //   setOpenInvoice(false);
-    // } catch (error) {
-    //   console.error("Error generating PDF:", error);
-    //   setOrderDownloading("");
-    //   setOpenInvoice(false);
-    // }
-  };
-
-  const generatePDF = (invoiceDetails, productDetails) => {
+const PDFGenerator = ({ order = {}, userName = "" }) => {
+  const generatePDF = (invoiceDetails) => {
     const documentDefinition = {
+      watermark: {
+        text: "Fresh Fare Shop",
+        color: "gray",
+        opacity: 0.1,
+        bold: true,
+        italics: false,
+        width: 100,
+        height: 100,
+        absolutePosition: { x: 200, y: 300 }, // Adjust the position of the watermark
+      },
       content: [
-        { text: "Invoice Details", style: "header" },
-        { text: "Invoice Number: " + invoiceDetails.invoiceNumber },
-        { text: "Date: " + invoiceDetails.date },
-        { text: "Customer: " + invoiceDetails.customer },
+        { text: "Invoice", style: "header", alignment: "center" },
+        {
+          text: [
+            { text: "Customer Name: ", bold: true },
+            { text: invoiceDetails.customerName },
+          ],
+        },
+        {
+          text: [
+            { text: "Order Date: ", bold: true },
+            { text: formatDate(invoiceDetails.orderDate) },
+          ],
+        },
+        {
+          text: [
+            { text: "Order #: ", bold: true },
+            { text: invoiceDetails.orderId },
+          ],
+        },
+        {
+          text: [
+            { text: "Invoice Date: ", bold: true },
+            { text: formatDate(new Date()) },
+          ],
+        },
         { text: "\n" },
-        { text: "Product Details", style: "header" },
+        {
+          canvas: [
+            {
+              type: "line",
+              x1: 0,
+              y1: 5,
+              x2: 515,
+              y2: 5,
+              lineWidth: 1,
+              lineColor: "#CCCCCC", // Gray color
+            },
+          ],
+        },
+        { text: "\n" },
+        { text: "Delivery Address: ", bold: true },
+        {
+          text: `${invoiceDetails.deliveryAddress.firstName} ${invoiceDetails.deliveryAddress.lastName}`,
+        },
+        { text: invoiceDetails.deliveryAddress.address },
+        { text: invoiceDetails.deliveryAddress.locality },
+        {
+          text: `${invoiceDetails.deliveryAddress.city} - ${invoiceDetails.deliveryAddress.pinCode}`,
+        },
+        { text: invoiceDetails.deliveryAddress.state },
+        { text: `Mobile #:${invoiceDetails.deliveryAddress.mobileNumber}` },
+        { text: "\n" },
+
         {
           table: {
             headerRows: 1,
-            widths: ["*", "*", "*"],
+            widths: ["*", "*", "*", "*"],
             body: [
-              ["Product", "Quantity", "Price"],
-              ...productDetails.map((product) => [
-                product.name,
+              [
+                { text: "Product", style: "tableHeader", bold: true },
+                { text: "Quantity", style: "tableHeader", bold: true },
+                { text: "Price", style: "tableHeader", bold: true },
+                { text: "Total", style: "tableHeader", bold: true },
+              ],
+              ...(invoiceDetails.items || []).map((product) => [
+                product.productName,
                 product.quantity,
-                product.price,
+                `$${product.price.toFixed(2)}`,
+                `$${(product.price * product.quantity).toFixed(2)}`,
               ]),
             ],
           },
+        },
+        { text: "\n" },
+        {
+          text: [
+            { text: "Total Amount: ", bold: true },
+            { text: `$${invoiceDetails.totalAmount.toFixed(2)}` },
+          ],
+          alignment: "right", // Aligning to the right
+          margin: [0, 10, 0, 0], // Adding margin to adjust spacing
         },
       ],
       styles: {
@@ -113,158 +109,58 @@ const PDFGenerator = ({
       },
     };
 
-    pdfMake.createPdf(documentDefinition).download("invoice.pdf");
+    pdfMake
+      .createPdf(documentDefinition)
+      .download(
+        `invoice-${order._id}-${moment().format("YYYY-MM-DD_HH-mm-ss")}.pdf`
+      );
   };
 
   const handleGeneratePDF = () => {
-    const invoiceDetails = {
-      invoiceNumber: "INV-001",
-      date: "2024-04-03",
-      customer: "John Doe",
-    };
+    let invoiceData = {};
+    if (order?.status === "delivered") {
+      const productSubtotals = (order) => {
+        return order.map((item) => item.price * item.quantity);
+      };
 
-    const productDetails = [
-      { name: "Product 1", quantity: 2, price: 10 },
-      { name: "Product 2", quantity: 1, price: 20 },
-      { name: "Product 3", quantity: 3, price: 15 },
-    ];
-    generatePDF(invoiceDetails, productDetails);
+      // Function to calculate total bill
+      const totalBill = (order) => {
+        return productSubtotals(order).reduce(
+          (acc, subtotal) => acc + subtotal,
+          0
+        );
+      };
+
+      // Construct order details object
+      invoiceData = {
+        customerName: userName,
+        items: order.items,
+        orderId: order._id,
+        orderDate: order.createdAt,
+        totalAmount: totalBill(order.items),
+        deliveryAddress: order.deliveryAddress,
+      };
+    }
+
+    generatePDF(invoiceData);
   };
 
   const formatDate = (dt) => {
     return moment(dt).format("DD-MM-YYYY HH:MM:SS");
   };
 
-  const handleCloseInvoice = () => {
-    setOpenInvoice(false);
-  };
-
   return (
     <div>
-      {orderDownloading === order?._id ? (
-        <LoadingSpinner
-          loadingMsg="Please wait. Downloading your order invoice..."
-          size="sm"
-        />
-      ) : order.status === "delivered" ? (
+      {order.status === "delivered" ? (
         <button
           onClick={() => handleGeneratePDF(true)}
           className="bg-blue-500 text-white px-4 py-2 rounded-md"
         >
-          Invoice
+          Download Invoice
         </button>
       ) : (
         <div></div>
       )}
-
-      <Modal
-        open={openInvoice || false}
-        onClose={handleCloseInvoice}
-        maxWidth="md"
-        fullWidth
-      >
-        <div className="bg-white rounded-md p-4 overflow-y-auto max-h-full">
-          <div className="flex justify-between">
-            {orderDownloading === order._id ? (
-              <LoadingSpinner
-                loadingMsg="Please wait. Downloading your order invoice..."
-                size="sm"
-              />
-            ) : order.status === "delivered" ? (
-              <button
-                onClick={() => handleDownloadInvoice()}
-                className="bg-blue-500 text-white px-4 py-2 rounded-md"
-              >
-                Download
-              </button>
-            ) : (
-              <div></div>
-            )}
-            <IconButton
-              // disabled={submittingAddress}
-              color="error"
-              onClick={handleCloseInvoice}
-              aria-label="delete"
-            >
-              <CloseIcon />
-            </IconButton>
-          </div>
-          {order.status === "delivered" && (
-            <div id="pdf-creation" ref={htmlRef}>
-              <div className="bg-white rounded-lg p-8">
-                <div className="text-center mb-8">
-                  <h1 className="text-2xl font-semibold text-gray-800">
-                    Invoice
-                  </h1>
-                </div>
-                <div className="mb-8">
-                  <p className="text-sm text-gray-700">
-                    <strong>Customer:</strong> {invoiceData.customerName}
-                  </p>
-                  <p className="text-sm text-gray-700">
-                    <strong>Order Date:</strong>{" "}
-                    {formatDate(invoiceData.createdAt)}
-                  </p>
-                  <p className="text-sm text-gray-700">
-                    <strong>Order #:</strong> {invoiceData.orderId}
-                  </p>
-                  <p className="text-sm text-gray-700">
-                    <strong>Invoice Date:</strong> {formatDate(new Date())}
-                  </p>
-                </div>
-                <div className="border-t border-gray-300"></div>
-                <div className="flex py-2 px-1  w-full md:w-full flex-1 md:flex-none">
-                  <div className="">
-                    <div className="font-semibold">Delivered Address:</div>
-                    <h4 className="">{`${invoiceData.deliveryAddress.firstName} ${invoiceData.deliveryAddress.lastName}`}</h4>
-
-                    <p className="">{invoiceData.deliveryAddress.address}</p>
-
-                    <p className="">{invoiceData.deliveryAddress.locality}</p>
-                    <p className="">
-                      {invoiceData.deliveryAddress.city} -{" "}
-                      {invoiceData.deliveryAddress.pinCode}
-                    </p>
-                    <p className="">{invoiceData.deliveryAddress.state}</p>
-
-                    <p className="">
-                      Mobile #:{invoiceData.deliveryAddress.mobileNumber}
-                    </p>
-                  </div>
-                </div>
-                <table className="w-full border-collapse mb-8">
-                  <thead>
-                    <tr>
-                      <th className="p-1 border">Product</th>
-                      <th className="p-1 border">Quantity</th>
-                      <th className="p-1 border">Price</th>
-                      <th className="p-1 border">Total</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {invoiceData.items.map((item) => (
-                      <tr key={item.productId}>
-                        <td className="p-1 border">{item.productName}</td>
-                        <td className="p-1 border">{item.quantity}</td>
-                        <td className="p-1 border">${item.price.toFixed(2)}</td>
-                        <td className="p-1 border">
-                          ${(item.quantity * item.price).toFixed(2)}
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-                <div className="text-right">
-                  <p className="text-lg font-semibold text-gray-800">
-                    <strong>Total Amount:</strong> $
-                    {invoiceData.totalAmount.toFixed(2)}
-                  </p>
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
-      </Modal>
     </div>
   );
 };
